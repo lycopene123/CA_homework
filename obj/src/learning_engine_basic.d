f424dc4452cbbd60eb9258c7b753db23a62b1478
lycopene123 <114247081+lycopene123@users.noreply.github.com>
2024-01-16 00:05:28 +0800

    Add files via upload
    update

---
+++ obj/src/learning_engine_basic.d
@@ -0,0 +1,38 @@
+obj/src/learning_engine_basic.o: src/learning_engine_basic.cc \
+ inc/learning_engine_basic.h inc/prefetcher.h inc/learning_engine_base.h \
+ inc/scooby.h inc/champsim.h inc/cache.h inc/memory_class.h inc/block.h \
+ inc/instruction.h inc/set.h inc/scooby_helper.h inc/bitmap.h \
+ inc/learning_engine_basic.h inc/learning_engine_featurewise.h \
+ inc/feature_knowledge.h inc/util.h
+
+inc/learning_engine_basic.h:
+
+inc/prefetcher.h:
+
+inc/learning_engine_base.h:
+
+inc/scooby.h:
+
+inc/champsim.h:
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
+
+inc/scooby_helper.h:
+
+inc/bitmap.h:
+
+inc/learning_engine_basic.h:
+
+inc/learning_engine_featurewise.h:
+
+inc/feature_knowledge.h:
+
+inc/util.h:
---
+++ obj/src/learning_engine_featurewise.d
@@ -0,0 +1,38 @@
+obj/src/learning_engine_featurewise.o: src/learning_engine_featurewise.cc \
+ inc/util.h inc/learning_engine_featurewise.h inc/learning_engine_base.h \
+ inc/prefetcher.h inc/feature_knowledge.h inc/scooby_helper.h \
+ inc/bitmap.h inc/scooby.h inc/champsim.h inc/cache.h inc/memory_class.h \
+ inc/block.h inc/instruction.h inc/set.h inc/learning_engine_basic.h \
+ inc/learning_engine_featurewise.h
+
+inc/util.h:
+
+inc/learning_engine_featurewise.h:
+
+inc/learning_engine_base.h:
+
+inc/prefetcher.h:
+
+inc/feature_knowledge.h:
+
+inc/scooby_helper.h:
+
+inc/bitmap.h:
+
+inc/scooby.h:
+
+inc/champsim.h:
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
+
+inc/learning_engine_basic.h:
+
+inc/learning_engine_featurewise.h:
